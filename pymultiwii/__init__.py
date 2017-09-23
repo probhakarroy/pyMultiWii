916619cfbfb1ea2e28634fc125fda9d2aaa64c2d
Probhakar Roy <31341278+probhakarroy@users.noreply.github.com>
2017-09-23 16:30:49 +0000

    Update __init__.py

--- pymultiwii/__init__.py
+++ pymultiwii/__init__.py
@@ -83,7 +83,7 @@ class MultiWii:
         self.ser.dsrdtr = False
         self.ser.writeTimeout = 2
         """Time to wait until the board becomes operational"""
-        wakeup = 2
+        wakeup = 14
         try:
             self.ser.open()
             if self.PRINT:
